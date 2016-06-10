1aff87332e938ce282000718708fc4244270fbc0
Gabe Lotto <glotto@cees-mazama.stanford.edu>
2016-06-10 16:35:33 -0700

    fixed RSLrate friction implementation

--- friction.f90
+++ friction.f90
@@ -787,8 +787,18 @@ contains
     absV = abs(V)
 
     fv = rs%f0 - (rs%b-rs%a)*log(absV/rs%V0)
-    alpha = rs%a*N/V*tanh(FR%Sf(i)/(rs%a*N))
-    beta = -V/rs%L*(FR%Sf(i)-N*fv)
+
+    if ( abs(rs%a*max(N,0d0)/FR%Sf(i)) < 1d-20 ) then 
+       if ( FR%Sf(i) > 0 ) then
+          alpha = rs%a*max(N,0d0)/V
+       else
+          alpha = -rs%a*max(N,0d0)/V
+       end if
+    else
+       alpha = rs%a*max(N,0d0)/V*tanh( FR%Sf(i)/(rs%a*max(N,0d0)) )
+    end if
+
+    beta = -absV/rs%L*( FR%Sf(i)-max(N,0d0)*sign(fv,FR%Sf(i)) )
 
     V = (Slock-FR%Sf(i)-dt*FR%DSf(i)+V*alpha+dt*alpha*FR%DV(i)-dt*beta)/(alpha+eta)
 
